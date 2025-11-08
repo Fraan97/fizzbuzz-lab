a84995a074796b4750e5b8dc7dce649ad4e660b9
Francesca Carcamo <francarcamolopez97@gmail.com>
2025-11-07 23:29:45 -0300

    feat: tarea 1 que imprime los números del 1 al 105

--- task1.js
+++ task1.js
@@ -1,5 +1,8 @@
 // Refer to Task 1 in your Instructions to complete this task
-
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task One!");
+for (let i = 1; i <= 105; i++) {
+    console.log(i);
   };
+//Inicialización: crea la variable i y la pone en 1. Es el punto de partida del bucle
+//Condición: antes de cada vuelta se comprueba si i es menor o igual a 105
+//Incremento: después de ejecutar el cuerpo del bucle en cada iteración, i++ suma 1 a i
+//Cuerpo: lo que está dentro de las llaves se ejecuta cada vez que la condición es true. Aquí imprime el valor actual de i en la consola.
